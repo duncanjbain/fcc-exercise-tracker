8a2535506b4c20d3b43cac3718da90b6767ebbe7
Duncan Bain <duncan@bain.io>
2020-02-27 23:13:00 +0000

    added mongodb connection

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require('dotenv').config();
 
 const app = express();
 
-require('dotenv').config();
+//  Set up mongoose connection to mongodb
+const mongoDB = process.env.MONGO_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('mongoDB succesfully connected');
+});
+
+db.on('error', () => {
+  console.error.bind(console, 'MongoDB connection error:');
+});
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.set('port', process.env.PORT || 3000);
 
@@ -10,6 +33,6 @@ const server = app.listen(app.get('port'), () => {
   console.log(`Express running on Port ${server.address().port}`);
 });
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send('Hello world!');
 });
