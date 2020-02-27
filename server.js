f8d0b753c9864c5763cadb38561a835439a2f361
Duncan Bain <duncan@bain.io>
2020-02-27 22:42:49 +0000

    npm dev depends added

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+
 const app = express();
 
-app.listen(3000, () => {
-    console.log('express listening on port 3000')
-})
+require('dotenv').config();
+
+app.set('port', process.env.PORT || 3000);
+
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running on Port ${server.address().port}`);
+});
 
 app.get('/', (req,res) => {
-    res.send('Hello world!')
-})
+  res.send('Hello world!');
+});
